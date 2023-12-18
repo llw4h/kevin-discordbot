4afbae248230bad4ba23957380121ad6712ec057
llw4h <30249831+llw4h@users.noreply.github.com>
2023-12-18 15:00:15 +0800

    updated to current version (2.1)
    needs to be updated to latest API

--- bot.js
+++ bot.js
@@ -1,62 +1,74 @@
+//discord test bot for server
 console.log("beep! boop! 🤖")
 
 require("dotenv").config();
 
+//import fetch from 'node-fetch'
 const fetch = require('node-fetch');
-
+//import keepAlive from './server'
+const keepAlive = require('./server');
+//import Discord from 'discord.js'
 const Discord = require('discord.js');
-const client = new Discord.Client();
-client.login(process.env.BOTTOKEN);
+const cron = require('cron');
+const { Client, Intents } = require('discord.js');
+const myIntents = new Intents();
+myIntents.add(Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES,Intents.FLAGS.GUILD_MEMBERS);
 
+//import DiscordJS, { Intents } from 'discord.js'
+//import dotenv from 'dotenv'
+
+const client = new Discord.Client({ intents: myIntents });
 //event handler
-client.on('ready', readyDiscord);
+//client.on('ready', readyDiscord);
+//function readyDiscord(){
+//  console.log('✨');
+//}
+client.on('ready', () => {
+  console.log('✨');
+});
 
-function readyDiscord(){
-    console.log('✨');
-}
+//test
+client.on('messageCreate', (message) => {
+  if (message.content === 'movie night?' || message.content === 'Movie night?'){
+    //message.reply({
+      //content: 'pong!!!',
+    //})
+    message.react('908566671457943552');
+    message.react('908566711870046300');
+  } 
+})
 
-const replies = [
-    'How may I serve you, master?',
-    'Good day!!',
-    "Hello, I'm a bot made by Lulu 🤖",
-    "I exist to serve. Beep Boop Beep."
-]
+//movie announcement
+let scheduledMessage = new cron.CronJob('00 00 10 * * 5,6,0', () => {
+  const guild = client.guilds.cache.get('756772478235181126');
+  const channel = guild.channels.cache.get('765894451025150003');
+  channel.send(`<@&${'759746575550513163'}> Anyone up for movie night?`);
+  message.react('908566671457943552');
+  message.react('908566711870046300');
+});
 
-client.on('message', gotMessage);
+// When you want to start it, use:
+scheduledMessage.start()
 
-async function gotMessage(msg){
-    console.log(msg.content);
-    if (msg.content == 'bot stop dos'){
-        msg.channel.send('@dos STOP DOS 🤺 STOP 🤺✋');
-    }
+client.on('guildMemberAdd', guildMember => {
+    guildMember.guild.channels.cache.get('798436413531881473').send(`<@${guildMember.user.id}> has joined the server!`);
+});
+client.on('guildMemberRemove', guildMember => {
+    guildMember.guild.channels.cache.get('798436413531881473').send(`<@${guildMember.user.id}> has left the server!`);
+});
 
-    else if(msg.content == 'rick roll me'){
-        let url = `https://g.tenor.com/v1/search?q=nevergonnagiveyouup&key=${process.env.TENORKEY}&limit=8`;
-        let response = await fetch(url);
-        let json = await response.json();
-        const index = Math.floor(Math.random() * json.results.length);
-        msg.channel.send(json.results[index].url);
-    }
 
-    // channel id is #bot-testing
-    else if (msg.channel.id == '841478585574490163' &&  msg.content == 'hello handy'){
-        const i = Math.floor(Math.random() * replies.length);
-        msg.reply(replies[i]);
-    }
-
-    else if (msg.content == 'bye handy'){
-        msg.reply("Of course, of course! Don't let me keep you!");
-    }
-
-    else if (msg.content == 'bot who is your creator'){
-        msg.reply('The almighty Lulu ✨✨');
-    }
-
-    else if (msg.content == 'good bot'){
-        msg.channel.send('💖');
-    }
-
-    else if (msg.content == 'bad bot'){
-        msg.channel.send('🤖✋');
-    }
-}
+//poll
+const { poll } = require('discord.js-poll');
+module.exports = {
+  name: 'poll',
+  description: 'Create a poll',
+  usage: 'Title + Option 1 + Option 2 + Option 3 + etc',
+  execute(client, message, args) {
+    poll(message, args, '+', '#00D1CD');
+    console.log('🤖');
+  },
+};
+//client.setMaxListeners(0)
+keepAlive();
+client.login(process.env.BOTTOKEN);
